0b356eb67a03eed8eb4a14495296a9f55eb43acf
Paige <pliu@microsoft.com>
2020-04-03 17:21:17 +0000

    Automl for aml 1.0.83

--- code/consts.py
+++ code/consts.py
@@ -1,6 +1,8 @@
 train_environment_name = 'greentaxi_train_env'
 inference_environment_name = 'greentaxi_inference_env'
 model_name = 'greentaxi_lgbm_pkl'
+model_name_automl = 'greentaxi_lgbm_pkl_automl'
 service_name = 'greentaxisvc'
+service_name_automl = 'greentaxisvcautoml'
 experiment_name = 'greentaxi_experiment'
 data_experiment_name = 'greentaxi_data_process'
